9e21a1222bbc154ba8473e922a7320c5649f1f24
Marc <mmaniscalco18@yahoo.com>
2013-06-25 19:50:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosie::Application.config.secret_token = 'f68a1d256e59315697110097346901d89351898e2bf340e2429cb90d7435a3ff280c936adb5e81cd2d57cfae7d7bb34ad70ee71e334b8cd4c525ff137c0a8083'
